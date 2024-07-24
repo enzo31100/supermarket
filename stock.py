94adcd720a3895ba288a68b0441955cc8b208160
Enzo Magnier <enzofegz@gmail.com>
2024-07-24 11:24:59 +0200

    Fichier stock

---
+++ stock.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+class stock:
+    def __init__(self, name, kilogram, quantity,piece):
+        self.name = name
+        self.kilogram = kilogram
+        self.quantity = quantity
+        self.piece = piece
+
+
+        inventory = {}
